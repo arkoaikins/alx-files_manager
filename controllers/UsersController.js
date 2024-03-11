0757fe38c34fc3399f46705bb5e8ce0a249939c8
arkoaikins <arkoaikins@gmail.com>
2024-03-11 07:59:11 -0700

    [task3]:Inside controllers, add a file UsersController.js that contains the new endpoint

---
+++ controllers/UsersController.js
@@ -0,0 +1,61 @@
+import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const userQueue = new Queue('email sending');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const userExists = await dbClient.client.db().collection('users').findOne({ email });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPassword = sha1(password);
+      const insertionInfo = await dbClient.client.db().collection('users').insertOne({ email, password: hashedPassword });
+      const userId = insertionInfo.insertedId.toString();
+
+      userQueue.add({ userId });
+
+      return res.status(201).json({ email, id: userId });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    const key = `auth_${token}`;
+
+    try {
+      const userId = await redisClient.get(key);
+      const userCollection = dbClient.client.db().collection('users');
+      const user = await userCollection.findOne({ _id: userId });
+
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      return res.json({ email: user.email, id: user._id });
+    } catch (error) {
+      console.error('Error getting user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+export default UsersController;
